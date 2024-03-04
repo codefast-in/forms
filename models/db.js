412ef9047593c841859642bef49b67b9af53796d
Dharmedra <dp903604@gmail.com>
2024-03-04 15:03:35 +0530

    update

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/formdata")
+  .connect(process.env.MONGODB_URI)
 
   .then(() => {
     console.log("Database connected successful");
